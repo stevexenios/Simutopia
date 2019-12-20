f4de45cfe24f6c810e8066ba2b59c27fd7a46e24
stevexenios <27645504+stevexenios@users.noreply.github.com>
2019-12-20 03:16:20 -0800

    Add files via upload

--- utilities.js
+++ utilities.js
@@ -38,30 +38,6 @@ function rgb(r, g, b) {
 	return "rgb(" + r + "," + g + "," + b + ")";
 }
 
-<<<<<<< HEAD
-=======
-//World
-var NUMBER_OF_CELLS = 10;
-var SIM_DURATION = 10000;
-var UPDATE_PERIOD = 20;
-var INITIAL_POPULATION = 100;
-var WORLD_DIFFICULTY = 65;
-//Cell
-var CELL_DIMENSION = 800 / NUMBER_OF_CELLS;
-var MAX_BONUS = 100;
-//Agent
-var DEATH_CHANCE = 0.01;
-var AGENT_DIMENSION = CELL_DIMENSION/5;
-var AGENT_COLOR = "red";
-//Gene
-var INDIVIDUAL_LEARNING = true;
-var SOCIAL_LEARNING = true;
-var BIOLOGICAL_LEARNING = true;
-var MUTATION_RATE = 0.05;
-var GENE_COUNT = 5;
-
-
->>>>>>> cf4aaa10aadfb97b74721e9afa5dcbfa6560ed07
 download_img = function (el) {
 	var image = canvas.toDataURL("image/jpg");
 	el.href = image;
