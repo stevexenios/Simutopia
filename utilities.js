3fbe4451a46d7d7600993d69359ca8b42483aa4c
stevexenios <27645504+stevexenios@users.noreply.github.com>
2020-02-29 19:52:10 -0800

    D

--- utilities.js
+++ utilities.js
@@ -26,8 +26,8 @@ var DIFFICULTY_DISTRIBUTION_CONSTANT = false; // Cells with Constant = Max World
 var DEATH_CHANCE = 0.01;
 var AGENT_DIMENSION = CELL_DIMENSION/4;
 var AGENT_COLOR = "red";
-var BASE_REPRODUCTION_COST = 10;
-var ENERGY_FACTOR = 3; // New addition
+var BASE_REPRODUCTION_COST = 1;
+var ENERGY_FACTOR = 5; // New addition
 
 //Gene
 var GENE_COUNT = 5;
@@ -44,7 +44,7 @@ var INDIVIDUAL_LEARNING_DAY = 0; // Start Day of Individual Learning Day
 var INDIVIDUAL_LEARNING = false; // Starts false, changed by world, depending on Indi..Day>=actual day
 var INDIVIDUAL_LEARNING_LABEL = 1;
 var I_GENOME_MUTATION_RATE = 0.05;
-var I_REPRODUCTION_FACTOR = 6;
+var I_REPRODUCTION_FACTOR = 1;
 var IL_RATE = 1.00;
 
 // Social Learning
@@ -56,7 +56,7 @@ var SOCIAL_LEARNING_RAND_AGENT = true; // SL Rand Agent
 var SOCIAL_LEARNING_DAY = 500; // Sets social learning to start at, or after this day
 var SOCIAL_LEARNING = false; // Starts false, changed by world, depending on Soci..Day>=actual day
 var SOCIAL_LEARNING_LABEL = 2;
-var S_REPRODUCTION_FACTOR = 6;
+var S_REPRODUCTION_FACTOR = 1;
 var SL_RATE = 1.00;
 
 // Bonus Distribution
